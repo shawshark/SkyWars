5e6fe32bdcacb24ec8a5c9b1c159a07c7b4bca27
Chris <chrissbolding@gmail.com>
2014-02-19 00:24:08 +1100

    Update Tasks.java

--- src/me/shawshark/skywars/Tasks.java
+++ src/me/shawshark/skywars/Tasks.java
@@ -1,7 +1,10 @@
 package me.shawshark.skywars;
 
+import me.confuser.barapi.BarAPI;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
@@ -50,6 +53,25 @@ public class Tasks {
 				
 				else if(count < 10 && count > 0) {
 					Bukkit.broadcastMessage(m.prefix + ChatColor.YELLOW + " The game will start in " + count + " seconds!");
+				}
+				
+				/* get all online players. */
+				for ( Player p : m.getServer().getOnlinePlayers() ) {
+					if(BarAPI.hasBar(p)) {
+						/* if players has bar from before, remove it. */
+						BarAPI.removeBar(p);
+					}
+					/* prepare the message */
+					String message = ChatColor.YELLOW + "The game will start in " + count + " seconds!";
+					/* set the message. */
+					BarAPI.setMessage(p, message);
+					if(count > 100) {
+						/* if the count timer is over 100, always set the boss health to 100 */
+						BarAPI.setHealth(p, 100);
+					} else {
+						/* if the count is under 100, set the boss health to the current timer. */
+						BarAPI.setHealth(p, count);
+					}
 				}
 				
 				/* take 1 from the count */
